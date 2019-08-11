5e7474d2110b0f4ff08af5e0e3e98b654c42e606
David Méndez Acuña <damenac@gmail.com>
2019-08-11 08:35:40 +0200

    fixing project structure

---
+++ lang/tests/test_Bella.py
@@ -0,0 +1,9 @@
+import unittest
+
+class test_Bella(unittest.TestCase):
+    
+    def test_UpOperation(self):
+        self.assertEqual("hola", "hola")
+
+    def test_DownOperation(self):
+        self.assertEqual("hola", "hola")
